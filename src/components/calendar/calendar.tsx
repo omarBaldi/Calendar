f37aca3fbc6d12187b597ad6498ccfc8d67af5a6
Omar Baldi <omb@netcompany.com>
2022-03-07 11:12:32 +0100

    Implemented functionality to re-build calendar whenever the user either goes backward or forward in month by pressing the buttons

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,20 +1,23 @@
 import { CSSProperties, FC, useEffect, useState } from 'react';
 import Styles from './calendar.module.scss';
-
+/**
+ * TODO: todo
+ * @returns
+ */
 const Calendar: FC<{}> = (): JSX.Element => {
+	const TODAY = new Date();
+	const WEEK_DAYS_NUMBER = 7;
 	interface CalendarDataI {
-		today: Date;
 		currentMonthDays: number;
-		currentYear: number;
+		currentYear?: number;
+		currentMonth?: number;
 	}
 
 	const [calendarData, setCalendarData] = useState<CalendarDataI>({
-		today: new Date(),
 		currentMonthDays: 0,
-		currentYear: 0,
+		currentYear: undefined,
+		currentMonth: undefined,
 	});
-
-	const WEEK_DAYS_NUMBER = 7;
 
 	/* Array of string containing the week days */
 	const weekDaysLabels: string[] = [
@@ -34,6 +37,25 @@ const Calendar: FC<{}> = (): JSX.Element => {
 	];
 
 	/* *------------------------------------------------------------------------------------------ Calendar logic */
+	enum ArrowFuncEnum {
+		BACKWARD = 0,
+		FORWARD = 1,
+	}
+
+	const handleButtonArrowClick = ({ action }: { action: ArrowFuncEnum }) => {
+		setCalendarData((prevState) => {
+			/* If the month is a number then get the previous state of the month and either subtract/sum the value of 1 to it,
+      otherwise if it is undefined, then */
+
+			const monthIndexValue: number =
+				prevState.currentMonth ?? TODAY.getMonth();
+			const valueToUpdate: number =
+				action === ArrowFuncEnum.BACKWARD
+					? monthIndexValue - 1
+					: monthIndexValue + 1;
+			return { ...prevState, currentMonth: valueToUpdate };
+		});
+	};
 
 	const updateCalendarData = ({
 		updatedData,
@@ -42,19 +64,26 @@ const Calendar: FC<{}> = (): JSX.Element => {
 	}): void => setCalendarData(updatedData);
 
 	const getTodayInformation = (): void => {
-		const { today } = calendarData;
-		const currentYear: number = today.getFullYear();
+		console.log('Calculate data for building calendar');
+
+		let updatedObj = {
+			/* Same for year */
+			currentYear: calendarData.currentYear ?? TODAY.getFullYear(),
+			/* If the current month is undefined, that means it is the first time that the component renders,
+      so get today's month otherwise calculate the days based on the prev saved month index */
+			currentMonth: calendarData.currentMonth ?? TODAY.getMonth(),
+		};
+
 		const monthDays: number = new Date(
-			currentYear,
-			today.getMonth() + 1,
+			updatedObj.currentYear,
+			updatedObj.currentMonth + 1,
 			0
 		).getDate();
 
 		updateCalendarData({
 			updatedData: {
-				today: calendarData.today,
 				currentMonthDays: monthDays,
-				currentYear,
+				...updatedObj,
 			},
 		});
 	};
@@ -62,9 +91,29 @@ const Calendar: FC<{}> = (): JSX.Element => {
 	/* As soon as the component renders for the first time, based on the current date,
   I want to retrieve how many days there are in the current month (in order to build the numbers UI)
   and the current year */
-	useEffect(getTodayInformation, []);
+	useEffect(getTodayInformation, [
+		calendarData.currentMonth,
+		calendarData.currentYear,
+	]);
 
 	/* *------------------------------------------------------------------------------------------ UI Calendar components */
+	/* TODO: Create component ad-hoc for this */
+	const ButtonArrow = ({
+		textLabel,
+		funcCallback,
+	}: {
+		textLabel: string;
+		funcCallback?: () => void;
+	}): JSX.Element => {
+		const handleButtonClick = () => funcCallback?.();
+
+		return (
+			<button onClick={handleButtonClick}>
+				<span>{textLabel}</span>
+			</button>
+		);
+	};
+
 	const Calendar = ({ currentMonthDays }: { currentMonthDays: number }) => {
 		/* Create an array of numbers containing all the days of the current month */
 		const arrayDaysMonth: number[] = [...Array(currentMonthDays)].map(
@@ -72,20 +121,42 @@ const Calendar: FC<{}> = (): JSX.Element => {
 		);
 
 		return (
-			<div
-				className={Styles.calendarElement}
-				style={{ '--weekDaysNumber': WEEK_DAYS_NUMBER } as CSSProperties}
-			>
-				{weekDays.map((weekDay: string, _: number) => (
-					<div key={weekDay} className={Styles.weekDay}>
-						{weekDay}
-					</div>
-				))}
-				{arrayDaysMonth.map((day: number, _: number) => (
-					<div key={day} className={Styles.day}>
-						<p>{day}</p>
-					</div>
-				))}
+			<div>
+				<div className={Styles.buttonArrowsRow}>
+					<ButtonArrow
+						textLabel='Go backward'
+						funcCallback={() =>
+							handleButtonArrowClick({ action: ArrowFuncEnum.BACKWARD })
+						}
+					/>
+
+					{/* Remove forward button from DOM if eventually the next month the user will select is greater
+          than the current month */}
+					{(calendarData.currentMonth ?? TODAY.getMonth()) + 1 <=
+						TODAY.getMonth() && (
+						<ButtonArrow
+							textLabel='Go forward'
+							funcCallback={() =>
+								handleButtonArrowClick({ action: ArrowFuncEnum.FORWARD })
+							}
+						/>
+					)}
+				</div>
+				<div
+					className={Styles.calendarElement}
+					style={{ '--weekDaysNumber': WEEK_DAYS_NUMBER } as CSSProperties}
+				>
+					{weekDays.map((weekDay: string, _: number) => (
+						<div key={weekDay} className={Styles.weekDay}>
+							{weekDay}
+						</div>
+					))}
+					{arrayDaysMonth.map((day: number, _: number) => (
+						<div key={day} className={Styles.day}>
+							<p>{day}</p>
+						</div>
+					))}
+				</div>
 			</div>
 		);
 	};
