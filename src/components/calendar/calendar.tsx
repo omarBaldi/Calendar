b4e3ec5b75e05bc61bdb15b26766f4a91c7dc9e7
Omar Baldi <omb@netcompany.com>
2022-03-07 12:19:08 +0100

    Implemented utility to create a custom array range of numbers and built a year selector based on that

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -6,6 +6,7 @@ import Styles from './calendar.module.scss';
  */
 const Calendar: FC<{}> = (): JSX.Element => {
 	const TODAY = new Date();
+	const DEFAULT_START_YEAR_OPTION = 1900;
 	const WEEK_DAYS_NUMBER = 7;
 	interface CalendarDataI {
 		currentMonthDays: number;
@@ -36,23 +37,57 @@ const Calendar: FC<{}> = (): JSX.Element => {
 		...weekDaysLabels.slice(0, 7),
 	];
 
+	/* An utils function that allows to build an array from a specific range */
+	/**
+	 *
+	 * @param start specify the value where we should start building the array from
+	 * @param end specify the value where we should stop populating the array with numbers
+	 * @returns an array filled with numbers from a specific range
+	 */
+	const buildArrayFromRange = ({
+		start = 0,
+		end,
+	}: {
+		start?: number;
+		end: number;
+	}): number[] => {
+		let currentArray: number[] = [];
+
+		const arraySize: number = end - start;
+
+		if (arraySize > 0) {
+			currentArray = [...Array(arraySize)].map((_, i: number) => i + start);
+		}
+
+		return [...currentArray, end];
+	};
+
 	/* *------------------------------------------------------------------------------------------ Calendar logic */
 	enum ArrowFuncEnum {
 		BACKWARD = 0,
 		FORWARD = 1,
 	}
 
+	const handleYearChange = (e: any) => {
+		const currentYearClicked: number = +e.target.value;
+		setCalendarData((prevState) => {
+			return { ...prevState, currentYear: currentYearClicked };
+		});
+	};
+
 	const handleButtonArrowClick = ({ action }: { action: ArrowFuncEnum }) => {
 		setCalendarData((prevState) => {
 			/* If the month is a number then get the previous state of the month and either subtract/sum the value of 1 to it,
       otherwise if it is undefined, then */
-
 			const monthIndexValue: number =
 				prevState.currentMonth ?? TODAY.getMonth();
+
+			//TODO: keep edge case in mind (when I reach a number lesser than 0, it should decrease the currentYear selected and vice-versa)
 			const valueToUpdate: number =
 				action === ArrowFuncEnum.BACKWARD
 					? monthIndexValue - 1
 					: monthIndexValue + 1;
+
 			return { ...prevState, currentMonth: valueToUpdate };
 		});
 	};
@@ -91,6 +126,8 @@ const Calendar: FC<{}> = (): JSX.Element => {
 	/* As soon as the component renders for the first time, based on the current date,
   I want to retrieve how many days there are in the current month (in order to build the numbers UI)
   and the current year */
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	useEffect(getTodayInformation, [
 		calendarData.currentMonth,
 		calendarData.currentYear,
@@ -101,24 +138,53 @@ const Calendar: FC<{}> = (): JSX.Element => {
 	const ButtonArrow = ({
 		textLabel,
 		funcCallback,
+		shouldBeHidden = false,
 	}: {
 		textLabel: string;
 		funcCallback?: () => void;
+		shouldBeHidden?: boolean;
 	}): JSX.Element => {
-		const handleButtonClick = () => funcCallback?.();
+		const handleButtonClick = (): void => {
+			if (!shouldBeHidden) funcCallback?.();
+		};
 
 		return (
-			<button onClick={handleButtonClick}>
+			<button
+				onClick={handleButtonClick}
+				style={{ cursor: shouldBeHidden ? 'not-allowed' : 'pointer' }}
+			>
 				<span>{textLabel}</span>
 			</button>
 		);
 	};
 
+	/* getFullYear() built-in method as a fallback */
+	const YearSelector = ({
+		currentYear = TODAY.getFullYear(),
+		yearsOptions,
+		callbackFunc,
+	}: {
+		currentYear?: number;
+		yearsOptions: number[];
+		callbackFunc?: (e: any) => void;
+	}) => {
+		return (
+			<select name='years' id='years' onChange={callbackFunc}>
+				{yearsOptions.map((year: number, _: number) => (
+					<option value={year} selected={year === currentYear}>
+						{year}
+					</option>
+				))}
+			</select>
+		);
+	};
+
 	const Calendar = ({ currentMonthDays }: { currentMonthDays: number }) => {
 		/* Create an array of numbers containing all the days of the current month */
-		const arrayDaysMonth: number[] = [...Array(currentMonthDays)].map(
-			(_, i) => i + 1
-		);
+		const arrayDaysMonth: number[] = buildArrayFromRange({
+			start: 1,
+			end: currentMonthDays,
+		});
 
 		return (
 			<div>
@@ -130,17 +196,28 @@ const Calendar: FC<{}> = (): JSX.Element => {
 						}
 					/>
 
-					{/* Remove forward button from DOM if eventually the next month the user will select is greater
-          than the current month */}
-					{(calendarData.currentMonth ?? TODAY.getMonth()) + 1 <=
-						TODAY.getMonth() && (
-						<ButtonArrow
-							textLabel='Go forward'
-							funcCallback={() =>
-								handleButtonArrowClick({ action: ArrowFuncEnum.FORWARD })
-							}
-						/>
-					)}
+					<YearSelector
+						{...{
+							currentYear: calendarData.currentYear,
+							/* An array filled with numbers needed to populate the HTML select-box */
+							yearsOptions: buildArrayFromRange({
+								start: DEFAULT_START_YEAR_OPTION,
+								end: TODAY.getFullYear(),
+							}),
+							callbackFunc: handleYearChange,
+						}}
+					/>
+
+					<ButtonArrow
+						textLabel='Go forward'
+						funcCallback={() =>
+							handleButtonArrowClick({ action: ArrowFuncEnum.FORWARD })
+						}
+						shouldBeHidden={
+							(calendarData.currentMonth ?? TODAY.getMonth()) + 1 >
+							TODAY.getMonth()
+						}
+					/>
 				</div>
 				<div
 					className={Styles.calendarElement}
